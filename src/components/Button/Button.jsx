e277d624276f913f8f826836f4541c98accb6555
Ogurcov Artem <ogurcov.artem2012@list.ru>
2022-08-17 12:31:45 +0300

    обернута в react memo

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,7 +1,8 @@
 import clsx from "clsx";
+import React from "react";
 import style from "./Button.module.css";
 
-const Button = ({ children, onClick, className, type, color, disabled = false }) => {
+const Button = React.memo(({ children, onClick, className, type, color, disabled = false }) => {
   const buttonClass = clsx(style.button, className, "button-active-effect", {
     [style.primary]: color === "primary",
     [style.secondary]: color === "secondary",
@@ -16,6 +17,6 @@ const Button = ({ children, onClick, className, type, color, disabled = false })
       {children}
     </button>
   );
-};
+});
 
 export default Button;
