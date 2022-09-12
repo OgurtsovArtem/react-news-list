0393467c56ce0a4f8c968117c0be370438a2e6fc
Ogurcov Artem <ogurcov.artem2012@list.ru>
2022-09-12 18:00:50 +0300

    Модуль сделан отдельным модальным окном

--- src/features/done/ui/index.tsx
+++ src/features/done/ui/index.tsx
@@ -1,15 +1,26 @@
+import { Modal } from "entities/modal";
 import { FC, MouseEventHandler } from "react";
+import { modelModal } from "shared/modal";
+import { AUTH_MODAL_ID } from "widgets/popups/lib/names";
 import style from "./style.module.css";
 
-const Done: FC<{ onClick: MouseEventHandler<HTMLButtonElement> }> = ({ onClick }) => {
+interface IDoneModalProps {
+  onClick?: MouseEventHandler<HTMLButtonElement>;
+  id: string | number;
+}
+
+const DoneModal: FC<IDoneModalProps> = ({ id }) => {
+  const onClick = () => modelModal.openModal(AUTH_MODAL_ID);
   return (
-    <div className={style.container}>
-      <legend className={style.title}>Пользователь успешно зарегистрирован!</legend>
-      <button className={style.changeFormButton} onClick={onClick} type="button">
-        Войти
-      </button>
-    </div>
+    <Modal id={id}>
+      <div className={style.container}>
+        <legend className={style.title}>Пользователь успешно зарегистрирован!</legend>
+        <button className={style.changeFormButton} onClick={onClick} type="button">
+          Войти
+        </button>
+      </div>
+    </Modal>
   );
 };
 
-export default Done;
+export default DoneModal;
